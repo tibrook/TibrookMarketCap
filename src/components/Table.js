90910db1f1581518900797dfc8caa1015a206ab5
tibrook <guillouet.alexandre35@gmail.com>
2022-09-15 16:14:42 +0200

    ajout tableau principal

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import TableLine from "./TableLine";
 
 const Table = ({ coinsData }) => {
   const [rangeNumber, setRangeNumber] = useState(100);
@@ -10,6 +11,7 @@ const Table = ({ coinsData }) => {
     "Volume",
     "1h",
     "1j",
+    "1s",
     "1m",
     "6m",
     "1a",
@@ -17,7 +19,7 @@ const Table = ({ coinsData }) => {
   ];
   return (
     <div className="table-container">
-      <div className="table-header">
+      <ul className="table-header">
         <div className="range-container">
           <span>
             Top{" "}
@@ -56,7 +58,11 @@ const Table = ({ coinsData }) => {
             <label htmlFor={el}>{el}</label>
           </li>
         ))}
-      </div>
+      </ul>
+      {coinsData &&
+        coinsData
+          .slice(0, rangeNumber)
+          .map((coin, index) => <TableLine coin={coin} index={index} />)}
     </div>
   );
 };
